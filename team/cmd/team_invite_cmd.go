1020155d725f49c6d0af32be15d00e5705bb3e20
Xavier Martínez Torres <xavier04.martinez@gmail.com>
2024-08-29 15:38:26 -0600

    refactor(team): cleanup invite command

--- team/cmd/team_invite_cmd.go
+++ team/cmd/team_invite_cmd.go
@@ -12,26 +12,21 @@ type TeamInviteCmd struct {
 	Targets []cmd.Target `cmd:"target"`
 }
 
-func (m TeamInviteCmd) Run(src cmd.Source, output *cmd.Output) {
+func (c TeamInviteCmd) Run(src cmd.Source, output *cmd.Output) {
 	// s means to self
 	if s, ok := src.(*player.Player); !ok {
 		output.Error("This command can only be run by a player.")
-	} else if len(m.Targets) == 0 {
+	} else if p := service.User().First(c.Targets); p == nil {
 		output.Error("No targets specified.")
 	} else if t := service.Team().LookupByMember(s.XUID()); t == nil {
 		output.Error(message.ErrSelfNotInTeam.Build())
-	} else if t.Role(s.XUID()).LowestThan(team.Officer) { // Check if the player is an officer or higher, if not, return an error
+	} else if r := t.Member(s.XUID()); r == team.Undefined {
+		output.Error(message.ErrSelfNotInTeam.Build())
+	} else if r.LowestThan(team.Officer) { // Check if the player is an officer or higher, if not, return an error
 		output.Error(message.ErrSelfNotOfficer.Build())
+	} else if err := service.Team().Invite(t, p); err != nil {
+		output.Error(err)
 	} else {
-		// lt means lazy target
-		for _, lt := range m.Targets {
-			if p, ok := lt.(*player.Player); ok {
-				if err := service.Team().Invite(t, p); err != nil {
-					output.Error(err)
-				} else {
-					s.Message(message.SuccessTeamInviteSent.Build(p.Name()))
-				}
-			}
-		}
+		s.Message(message.SuccessTeamInviteSent.Build(p.Name()))
 	}
 }
