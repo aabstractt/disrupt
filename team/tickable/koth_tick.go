b8be04ea5252c1ecbd2a74f75387de49d6596506
Abstract <xavier04.martinez@gmail.com>
2024-08-30 19:24:19 -0600

    Update koth_tick.go

--- team/tickable/koth_tick.go
+++ team/tickable/koth_tick.go
@@ -1,67 +1,67 @@
 package tickable
 
 import (
-	"github.com/bitrule/disrupt"
-	"github.com/bitrule/disrupt/service"
-	"github.com/df-mc/dragonfly/server/block/cube"
-	"github.com/df-mc/dragonfly/server/player"
-	"time"
+    "github.com/bitrule/disrupt"
+    "github.com/bitrule/disrupt/service"
+    "github.com/df-mc/dragonfly/server/block/cube"
+    "github.com/df-mc/dragonfly/server/player"
+    "time"
 )
 
 type KoTHTick struct {
-	teamId string
+    teamId string
 
-	duration time.Duration // Duration of the KoTH
-	bbox     cube.BBox
+    duration time.Duration // Duration of the KoTH
+    bbox     cube.BBox
 
-	capturingAt time.Time // Time the KoTH is being captured
-	capturingBy string    // Player capturing the KoTH
+    capturingAt time.Time // Time the KoTH is being captured
+    capturingBy string    // Player capturing the KoTH
 }
 
 // Remaining returns the remaining time of the KoTH.
 func (kt *KoTHTick) Remaining() time.Duration {
-	return kt.duration - time.Since(kt.capturingAt)
+    return kt.duration - time.Since(kt.capturingAt)
 }
 
 // Tick ticks the KoTH.
 func (kt *KoTHTick) Tick() {
-	t := service.Team().LookupById(kt.teamId)
-	if t == nil {
-		disrupt.Log.Panic("KoTH team not found")
+    t := service.Team().LookupById(kt.teamId)
+    if t == nil {
+        disrupt.Log.Panic("KoTH team not found")
 
-		return
-	}
+        return
+    }
 
-	for wName := range t.Tracker().Cuboids() {
-		w := service.World().LookupByName(wName)
-		if w == nil {
-			disrupt.Log.WithField("world", wName).Error("KoTH world not found")
+    for wName := range t.Tracker().Cuboids() {
+        w := service.World().LookupByName(wName)
+        if w == nil {
+            disrupt.Log.WithField("world", wName).Error("KoTH world not found")
 
-			continue
-		}
+            continue
+        }
 
-		if kt.capturingBy != "" {
-			if p, ok := disrupt.SRV.PlayerByXUID(kt.capturingBy); ok && p.World() == w && kt.bbox.Vec3Within(p.Position()) {
-				return
-			}
-		}
+        if kt.capturingBy != "" {
+            if p, ok := disrupt.SRV.PlayerByXUID(kt.capturingBy); ok && p.World() == w && kt.bbox.Vec3Within(p.Position()) {
+                return
+            }
+        }
 
-		for _, e := range w.EntitiesWithin(kt.bbox, nil) {
-			if p, ok := e.(*player.Player); ok {
-				pt := service.Team().LookupByMember(p.XUID())
-				if pt == nil {
-					continue
-				}
+        for _, e := range w.EntitiesWithin(kt.bbox, nil) {
+            if p, ok := e.(*player.Player); ok {
+                pt := service.Team().LookupByMember(p.XUID())
+                if pt == nil {
+                    continue
+                }
 
-				kt.capturingAt = time.Now()
-				kt.capturingBy = p.XUID()
+                kt.capturingAt = time.Now()
+                kt.capturingBy = p.XUID()
 
-				// TODO: Broadcast message
+                // TODO: Broadcast message
 
-				break
-			}
-		}
-	}
+                break
+            }
+        }
+    }
 
-	// TODO: Tick capturing
+    // TODO: Tick capturing
 }
